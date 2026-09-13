841ae55a704406f68d5d928bea269ba63ea32b97
ShazaElsayedIsmail11 <elsayedshaza9@gmail.com>
2026-09-13 12:38:05 +0300

    Fix CV upload for Vercel

--- middlewares/uploadCV.ts
+++ middlewares/uploadCV.ts
@@ -1,18 +1,45 @@
+// import multer from "multer";
+// import AppError from "../utils/AppError.js";
+
+// const uploadCV=multer({
+// dest: "uploads/cvs/" ,
+// limits: {
+//     fileSize: 5 * 1024 * 1024,
+//   } ,
+// fileFilter:(req, file, cb) => {
+//     if (file.mimetype !== "application/pdf") {
+// return cb(
+//   new AppError("Only PDF files are allowed", 400)
+// );    }
+
+//     cb(null, true);
+//   },
+// })
+// export default uploadCV
 import multer from "multer";
 import AppError from "../utils/AppError.js";
 
-const uploadCV=multer({
-dest: "uploads/cvs/" ,
-limits: {
+const uploadDirectory =
+  process.env.VERCEL === "1"
+    ? "/tmp"
+    : "uploads/cvs/";
+
+const uploadCV = multer({
+  dest: uploadDirectory,
+
+  limits: {
     fileSize: 5 * 1024 * 1024,
-  } ,
-fileFilter:(req, file, cb) => {
+  },
+
+  fileFilter: (req, file, cb) => {
     if (file.mimetype !== "application/pdf") {
-return cb(
-  new AppError("Only PDF files are allowed", 400)
-);    }
+      return cb(
+        new AppError("Only PDF files are allowed", 400)
+      );
+    }
 
     cb(null, true);
   },
-})
-export default uploadCV
+});
+
+export default uploadCV;
